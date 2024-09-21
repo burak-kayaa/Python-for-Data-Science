61481643e88bccfd92d6878cc9f1d98c9e36d686
burak-kayaa <burakkaya_2004@hotmail.com>
2024-09-21 17:07:05 +0300

    fix

--- Python-0-Starting/ex02/find_ft_type.py
+++ Python-0-Starting/ex02/find_ft_type.py
@@ -1,9 +1,16 @@
 def all_thing_is_obj(object: any) -> int:
+    minishell = {
+        "List": "list",
+        "Tuple": "tuple",
+        "Dict": "dict",
+        "Set": "set",
+        "Str": "str",
+    }
     typ = type(object)
     name = typ.__name__
     name = name[0].upper() + name[1:]
     # name = typeps.get(typ.__name__, None)
-    if name == "Int" or name == "Float":
+    if minishell.get(name) is None:
         print("Type not found")
     elif name == "Str":
         print(f"{object} is in the kitchen : {object.__class__}")   
