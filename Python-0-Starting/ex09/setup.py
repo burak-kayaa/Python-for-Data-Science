0df9ad5f4565dd433a2f9f0762d11796aabaaed8
burak-kayaa <burakkaya_2004@hotmail.com>
2024-09-21 17:02:49 +0300

    fix

--- Python-0-Starting/ex09/setup.py
+++ Python-0-Starting/ex09/setup.py
@@ -1,21 +1,17 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="package",  # Paketinizin adı
-    version="0.1",  # Sürüm numarası
+    name="ft_package",  # Paketinizin adı (pip show'daki Name)
+    version="0.0.1",  # Sürüm numarası (pip show'daki Version)
     packages=find_packages(),  # Paketleri otomatik bulmak için
     include_package_data=True,  # README.md, LICENSE gibi dosyaları dahil etmek için
-    install_requires=[],  # Gerekli bağımlılıkları ekleyebilirsiniz
-    description="A simple example package",
+    install_requires=[],  # Bağımlılıklar varsa buraya eklenir
+    description="A sample test package",  # Kısa açıklama (pip show'daki Summary)
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",  # PyPI'de README.md'yi Markdown olarak göstermek için
-    author="Burak KAYA",
-    author_email="burakkaya_2004@hotmail.com",
-    url="https://github.com/username/package",  # Projenizin URL'si (GitHub, Bitbucket, vb.)
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
+    author="eagle",  # Yazar adı (pip show'daki Author)
+    author_email="eagle@42.fr",  # Yazar e-posta adresi (pip show'daki Author-email)
+    url="https://github.com/eagle/ft_package",  # Proje URL'si (pip show'daki Home-page)
     python_requires='>=3.6',  
+    license="MIT",  # Lisans türü (pip show'daki License)
 )
