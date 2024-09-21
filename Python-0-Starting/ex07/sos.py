4b8495f15336b16ca2c5571715765f4e331af63a
burak-kayaa <burakkaya_2004@hotmail.com>
2024-09-21 17:26:05 +0300

    fix

--- Python-0-Starting/ex07/sos.py
+++ Python-0-Starting/ex07/sos.py
@@ -1,18 +1,30 @@
 import sys
 
+
 def arg_checher(args):
+    """
+    Check if the arguments are valid
+    """
     assert len(args) == 2, "AssertionError: the arguments are bad"
     assert args[1].isalnum(), "AssertionError: the arguments are bad"
 
+
 def main():
+    """
+    Main function
+    """
     try:
         arg_checher(sys.argv)
+        sos(sys.argv[1])
     except AssertionError as e:
         print(e)
         return
-    sos(sys.argv[1])
+
 
 def sos(string):
+    """
+    Print the morse code of a string
+    """
     NESTED_MORSE = {
         " ": "/ ",
         "A": ".- ",
@@ -52,9 +64,11 @@ def sos(string):
         "8": "---.. ",
         "9": "----. ",
     }
+    assert string in NESTED_MORSE, "AssertionError: the arguments are bad"
     for char in string:
-        print(NESTED_MORSE[char.upper()], end="")
+        print(NESTED_MORSE.get(char.upper(), ""), end="")
     print()
+
 
 if __name__ == '__main__':
     main()
